65fb1044009e796deeb7865f7d930b30fe29a01e
dungpb <badungphan99@gmail.com>
2020-08-27 21:19:06 +0700

    add menuitem

---
+++ libGui/menuItem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dungpb on 27/08/2020.
+//
+
+#include "menuItem.h"
---
+++ libGui/menuItem.h
@@ -0,0 +1,20 @@
+//
+// Created by dungpb on 27/08/2020.
+//
+
+#ifndef AUTOUPLOADDATA_MENUITEM_H
+#define AUTOUPLOADDATA_MENUITEM_H
+
+#include <string>
+#include <vector>
+
+class menuItem {
+private:
+    std::string _title;
+    std::vector<menuItem> _menuItems;
+
+public:
+};
+
+
+#endif //AUTOUPLOADDATA_MENUITEM_H
